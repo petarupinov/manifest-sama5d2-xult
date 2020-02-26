d300b0c9e4c90fa29b639b992f3d2b1670d0b00b
Petar Upinov <petar.upinov@gmail.com>
2020-02-26 23:00:27 +0200

    script: update add output logs

--- sama5d2-xplained-ultra-linux-release.sh
+++ sama5d2-xplained-ultra-linux-release.sh
@@ -11,30 +11,30 @@ cd at91bootstrap/
 git checkout remotes/linux4sam/at91bootstrap-3.x -b base
 git checkout -b dev
 git reset --hard v3.9.0
-make clean && make mrproper && make ARCH=arm CROSS_COMPILE=arm-none-eabi- distclean
-make ARCH=arm CROSS_COMPILE=arm-none-eabi- sama5d2_xplaineddf_uboot_defconfig
-make ARCH=arm CROSS_COMPILE=arm-none-eabi-
+make clean && make mrproper && make ARCH=arm CROSS_COMPILE=arm-none-eabi- distclean | tee clean.log
+make ARCH=arm CROSS_COMPILE=arm-none-eabi- sama5d2_xplaineddf_uboot_defconfig | tee config.log
+make ARCH=arm CROSS_COMPILE=arm-none-eabi- | tee build.log
 # 3. U-Boot - BOOTLOADER
 cd ../u-boot-at91/
 git checkout remotes/linux4sam/u-boot-2019.04-at91 -b base
 git checkout -b dev
 git reset --hard linux4sam_6.2
-make clean && make mrproper && make ARCH=arm CROSS_COMPILE=arm-none-eabi- distclean
-make ARCH=arm CROSS_COMPILE=arm-none-eabi- sama5d2_xplained_spiflash_defconfig
-make ARCH=arm CROSS_COMPILE=arm-none-eabi-
+make clean && make mrproper && make ARCH=arm CROSS_COMPILE=arm-none-eabi- distclean | tee clean.log
+make ARCH=arm CROSS_COMPILE=arm-none-eabi- sama5d2_xplained_spiflash_defconfig | tee config.log
+make ARCH=arm CROSS_COMPILE=arm-none-eabi- | tee build.log
 cp ../u-boot-env.txt .
-./tools/mkenvimage -s 0x2000 -o u-boot-env.bin u-boot-env.txt
+./tools/mkenvimage -s 0x2000 -o u-boot-env.bin u-boot-env.txt | tee build_env.log
 # 4. Kernel
 cd ../ && cd linux-at91/
 git checkout remotes/linux4sam/linux-4.19-at91 -b base
 git checkout -b dev
 git reset --hard linux4sam_6.2
-make clean && make mrproper && make ARCH=arm CROSS_COMPILE=arm-none-eabi- distclean
-make ARCH=arm CROSS_COMPILE=arm-none-eabi- sama5_defconfig
-make ARCH=arm CROSS_COMPILE=arm-none-eabi- -j$(nproc)
+make clean && make mrproper && make ARCH=arm CROSS_COMPILE=arm-none-eabi- distclean | tee clean.log
+make ARCH=arm CROSS_COMPILE=arm-none-eabi- sama5_defconfig | tee config.log
+make ARCH=arm CROSS_COMPILE=arm-none-eabi- -j$(nproc) | tee build.log
 # 5. Device Tree Overlays (DTO) and Flattened Image Tree (FIT)
 cd ../dt-overlay-at91/
 export KERNEL_DIR=../linux-at91/
-make ARCH=arm CROSS_COMPILE=arm-none-eabi- sama5d2_xplained_dtbos
-make ARCH=arm CROSS_COMPILE=arm-none-eabi- sama5d2_xplained.itb
+make ARCH=arm CROSS_COMPILE=arm-none-eabi- sama5d2_xplained_dtbos | tee build_dtbos.log
+make ARCH=arm CROSS_COMPILE=arm-none-eabi- sama5d2_xplained.itb | tee build_itb.log
 cd ../
